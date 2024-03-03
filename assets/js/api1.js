8357e09c6b7898d82ba10924674a7dcbde39bb0c
Belalalimam <bb5503344@gmail.com>
2024-03-03 02:31:44 +0200

    The-Website-Of-Quran

---
+++ assets/js/api1.js
@@ -0,0 +1,17 @@
+// const apiUrl = "https://mp3quran.net/api/v3/suwar?language=eng";
+
+// async function get(){
+//     const chooseSurah = document.querySelector("#chooseSurah")
+//     const Sur = await fetch(`${apiUrl}/suwar`)
+//     const data = await Sur.json()
+//     console.log(data)
+
+
+//     data.suwar.forEach(suwar => chooseSurah.innerHTML += `<Option value = "${suwar.id}">${suwar.name}</Option>`);
+//     // chooseSurah.addEventListener('change', Q => getSurah(Q.target.value))
+// }
+// get()
+// // function getSurah(suwar){
+// //     console.log(suwar)
+
+// // }
