1ed2cfb436b666a4f8e366cfaf9d7d90c864886a
Belalalimam <bb5503344@gmail.com>
2024-04-26 02:44:43 +0300

    ip

---
+++ assets/js/Boycott.js
@@ -0,0 +1,56 @@
+fetch('data.json')
+.then(response => response.json())
+.then((data) => show(data))
+
+
+function show(data){
+
+    let table_head = '';
+    for(i in data[0]){
+        table_head += `<th>${i}</th>`;
+    }
+    table_head = `<thead><tr>${table_head}</tr></thead>`
+
+
+    let table_body = '';
+
+    // for(j in data){
+    //     for(i in data[j]){
+    //         console.log(i)
+    //     }
+    // }
+
+    for(i in data){
+        
+        table_body += `
+        <tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].company}</td>
+            <td>${data[i].product}</td>
+            <td><img src="${data[i].img}" width="200px"></td>
+        </tr>
+        `;
+    }
+    table_body = `<tbody><tr>${table_body}</tr></tr></tbody>`
+
+
+
+
+
+
+
+
+
+    const tabel = document.querySelector("#tabel-container");
+
+    tabel.innerHTML = 
+    `
+    <table class="table table-striped" id="data-table">
+        ${table_head}
+        ${table_body}
+    </table>
+    `;
+
+}
+   
